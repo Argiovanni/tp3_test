af4253f03c3a2912bf79feb0e8974bcb8ec673d0
Argiovanni <arthur.bongiovanni@gmail.com>
2023-04-20 16:26:47 +0200

    initial commit

---
+++ src/ilu2/repertoireTest/WelcomeTest.java
@@ -0,0 +1,14 @@
+package ilu2.repertoireTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
